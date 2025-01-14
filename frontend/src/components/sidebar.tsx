3e3006b18c68d7e20620ada59a0e114de207af27
Jan Krepl <18519371+jankrepl@users.noreply.github.com>
2025-01-14 10:58:58 +0100

    Add new icons

--- frontend/src/components/sidebar.tsx
+++ frontend/src/components/sidebar.tsx
@@ -1,11 +1,17 @@
-import { Search } from "lucide-react";
+import { PanelRightOpen, Search, SquarePen } from "lucide-react";
 import { ThreadCardSidebar } from "@/components/thread-card-sidebar";
 
 export function Sidebar() {
   return (
     <div className="w-[15%] border-r-2">
-      <div className="flex justify-end p-4">
-        <Search size={24} />
+      <div className="flex justify-between">
+        <div className="p-4">
+          <PanelRightOpen />
+        </div>
+        <div className="flex p-4 gap-4 ">
+          <Search size={24} />
+          <SquarePen size={24} />
+        </div>
       </div>
       <p className="text-xl p-4 text-center">Conversations</p>
       <div className="flex flex-col gap-2 pl-3">
