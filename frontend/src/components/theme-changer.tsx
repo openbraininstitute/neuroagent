169a49bd4bc182bb9deb2521a495a34d6f1b12eb
Boris Bergsma <boris.bergsma@openbraininstitute.org>
2025-01-28 16:49:40 +0100

    final fixes

--- frontend/src/components/theme-changer.tsx
+++ frontend/src/components/theme-changer.tsx
@@ -7,8 +7,6 @@ import * as React from "react";
 import { Moon, Sun } from "lucide-react";
 import { useTheme } from "next-themes";
 
-import { Button } from "@/components/ui/button";
-
 export default function ThemeChanger({
   children,
 }: {
@@ -41,17 +39,11 @@ export function ThemeToggle() {
   const { theme, setTheme } = useTheme();
 
   return (
-    <Button
-      variant="link"
-      size="icon"
+    <button
       className="hover:scale-[1.1] transition border-none"
       onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
     >
-      {theme === "dark" ? (
-        <Sun className="h-[100%] rotate-0 scale-100 transition-all" />
-      ) : (
-        <Moon className="h-[100%] rotate-0 scale-100 transition-all" />
-      )}
-    </Button>
+      {theme === "dark" ? <Sun /> : <Moon />}
+    </button>
   );
 }
