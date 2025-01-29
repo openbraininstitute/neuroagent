9821332a232a01af6c71790b04903104c0027b10
Boris Bergsma <boris.bergsma@openbraininstitute.org>
2025-01-29 10:32:38 +0100

    Good for merge

--- frontend/src/components/sidebar.tsx
+++ frontend/src/components/sidebar.tsx
@@ -8,8 +8,10 @@ import {
   SquarePen,
 } from "lucide-react";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export function Sidebar({ children }: { children: React.ReactNode }) {
+  const pathname = usePathname();
   const [showSidebar, setShowSidebar] = useState(true);
 
   const toggleSidebar = () => {
@@ -18,40 +20,42 @@ export function Sidebar({ children }: { children: React.ReactNode }) {
 
   return (
     <>
-      <div className="relative">
-        <div
-          className={`absolute top-0 left-0 h-full border-r-2 shadow-md transition-transform duration-300 ${
-            showSidebar ? "translate-x-0" : "-translate-x-full"
-          }`}
-        >
-          <div className="flex justify-between border-b-2">
-            <div className="p-4">
-              <PanelRightOpen
-                className="hover:scale-[1.1] transition"
-                onClick={toggleSidebar}
-              />
+      {pathname !== "/login" ? (
+        <div className="relative">
+          <div
+            className={`absolute top-0 left-0 h-full border-r-2 shadow-md transition-transform duration-300 ${
+              showSidebar ? "translate-x-0 relative" : "-translate-x-full"
+            }`}
+          >
+            <div className="flex justify-between border-b-2">
+              <div className="p-4">
+                <PanelRightOpen
+                  className="hover:scale-[1.1] transition"
+                  onClick={toggleSidebar}
+                />
+              </div>
+              <div className="flex p-4 gap-4">
+                <Search className="hover:scale-[1.1] transition" />
+                <Link href="/">
+                  <SquarePen className="hover:scale-[1.1] transition" />
+                </Link>
+              </div>
             </div>
-            <div className="flex p-4 gap-4">
-              <Search className="hover:scale-[1.1] transition" />
-              <Link href="/">
-                <SquarePen className="hover:scale-[1.1] transition" />
-              </Link>
-            </div>
+            <div className="opacity-50 my-4"></div>
+            <div className="overflow-auto flex-1">{children}</div>
           </div>
-          <div className="opacity-50 my-4"></div>
-          <div className="overflow-auto flex-1">{children}</div>
-        </div>
-        <div
-          className={`p-4 relative left-0 top-0 border-r-2 h-full transition-opacity duration-1000
+          <div
+            className={`p-4 relative left-0 top-0 border-r-2 h-full transition-opacity duration-1000
     ${showSidebar ? "opacity-0 invisible" : "opacity-100 visible"}`}
-        >
-          <PanelRightClose
-            className="hover:scale-[1.1] transition"
-            size={24}
-            onClick={toggleSidebar}
-          />
+          >
+            <PanelRightClose
+              className="hover:scale-[1.1] transition"
+              size={24}
+              onClick={toggleSidebar}
+            />
+          </div>
         </div>
-      </div>
+      ) : null}
     </>
   );
 }
