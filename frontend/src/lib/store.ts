0741bcea2686f01972b32ef585f22f768466ef4a
Jan Krepl <18519371+jankrepl@users.noreply.github.com>
2025-02-05 23:27:12 +0100

    Undo persisting zustand store to local storage

--- frontend/src/lib/store.ts
+++ frontend/src/lib/store.ts
@@ -1,19 +1,11 @@
 import { create } from "zustand";
-import { persist } from "zustand/middleware";
 
 interface StoreState {
   newMessage: string;
   setNewMessage: (message: string) => void;
 }
 
-export const useStore = create<StoreState>()(
-  persist(
-    (set) => ({
-      newMessage: "",
-      setNewMessage: (message) => set({ newMessage: message }),
-    }),
-    {
-      name: "store", // unique name for the storage
-    },
-  ),
-);
+export const useStore = create<StoreState>((set) => ({
+  newMessage: "",
+  setNewMessage: (message) => set({ newMessage: message }),
+}));
