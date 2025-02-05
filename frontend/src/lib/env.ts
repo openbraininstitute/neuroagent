e2255446a5fda8028735f59e0e75ad89c6868c7f
Jan Krepl <18519371+jankrepl@users.noreply.github.com>
2025-02-05 21:14:50 +0100

    Progress

--- frontend/src/lib/env.ts
+++ frontend/src/lib/env.ts
@@ -5,25 +5,22 @@ export const env = createEnv({
   server: {
     SERVER_SIDE_BACKEND_URL: z.string().url().optional(),
     NEXTAUTH_SECRET: z.string().min(1),
-    NEXTAUTH_URL: z.string().url(),
+    KEYCLOAK_ID: z.string().min(1),
+    KEYCLOAK_SECRET: z.string().min(1),
+    KEYCLOAK_ISSUER: z.string().url(),
   },
   client: {
     NEXT_PUBLIC_BACKEND_URL: z.string().url(),
-    NEXT_PUBLIC_KEYCLOAK_ID: z.string().min(1),
-    NEXT_PUBLIC_KEYCLOAK_SECRET: z.string().min(1),
-    NEXT_PUBLIC_KEYCLOAK_ISSUER: z.string().url(),
   },
   runtimeEnv: {
-    // Server vars - fallback to client URL if server URL not provided
-    SERVER_SIDE_BACKEND_URL:
-      process.env.SERVER_SIDE_BACKEND_URL ||
-      process.env.NEXT_PUBLIC_BACKEND_URL,
+    // Server vars
+    SERVER_SIDE_BACKEND_URL: process.env.SERVER_SIDE_BACKEND_URL,
     NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
-    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+    KEYCLOAK_ID: process.env.KEYCLOAK_ID,
+    KEYCLOAK_SECRET: process.env.KEYCLOAK_SECRET,
+    KEYCLOAK_ISSUER: process.env.KEYCLOAK_ISSUER,
     // Client vars
     NEXT_PUBLIC_BACKEND_URL: process.env.NEXT_PUBLIC_BACKEND_URL,
-    NEXT_PUBLIC_KEYCLOAK_ID: process.env.NEXT_PUBLIC_KEYCLOAK_ID,
-    NEXT_PUBLIC_KEYCLOAK_SECRET: process.env.NEXT_PUBLIC_KEYCLOAK_SECRET,
-    NEXT_PUBLIC_KEYCLOAK_ISSUER: process.env.NEXT_PUBLIC_KEYCLOAK_ISSUER,
   },
+  skipValidation: true,
 });
