71d3fd43ef06365025f4ce825a80cd59a2ed7654
Boris Bergsma <boris.bergsma@openbraininstitute.org>
2025-02-07 16:46:45 +0100

    fix layout shift between pages

--- frontend/src/components/chat-input-loading.tsx
+++ frontend/src/components/chat-input-loading.tsx
@@ -10,32 +10,31 @@ export default function ChatInputLoading({ newMessage }: ChatComponentProps) {
   return (
     <div className="flex flex-col h-full">
       <div className="relative flex justify-center items-center p-6 w-full">
+        <h1 className="text-3xl absolute"></h1>
         <Button className="hover:scale-105 active:scale-[1.10] ml-auto">
           Hide Tools
         </Button>
       </div>
       <div className="flex-1 flex flex-col overflow-y-auto my-4">
-        <div className="flex flex-col">
-          <ChatMessageHuman
-            key="temp"
-            id="temp"
-            threadId="temp"
-            content={newMessage}
-          />
-        </div>
+        <ChatMessageHuman
+          key="temp"
+          id="temp"
+          threadId="temp"
+          content={newMessage}
+        />
       </div>
       <form className="flex flex-col justify-center items-center gap-4 mb-4">
-        <div className="relative w-1/2">
+        <div className="flex items-center min-w-[70%] max-w-[100%] border-2 border-gray-500 rounded-full overflow-hidden">
           <input
             type="text"
             readOnly
-            className="border-2 border-gray-500 w-full p-4 rounded-full"
+            className="flex-grow outline-none w-full p-4 bg-transparent"
             name="prompt"
             placeholder="Message the AI..."
             autoComplete="off"
           />
-          <div className="absolute right-4 top-1/2 -translate-y-1/2">
-            <div className="w-4 h-4 border-2 border-gray-500 border-t-transparent rounded-full animate-spin" />
+          <div className="flex gap-2 mr-3">
+            <div className="w-6 h-6 border-2 ml-2 p-1 border-gray-500 border-t-transparent rounded-full animate-spin" />
           </div>
         </div>
       </form>
