0a67b14e4d3b203d7559ff29877c665875f7e072
Boris Bergsma <boris.bergsma@openbraininstitute.org>
2025-02-07 14:15:46 +0100

    change width messages

--- frontend/src/components/chat-message-ai.tsx
+++ frontend/src/components/chat-message-ai.tsx
@@ -12,7 +12,7 @@ export function ChatMessageAI({ content }: ChatMessageAIProps) {
   return (
     <div className="border-r-2 p-8 border-white-300 border-solid">
       <div className="flex justify-start">
-        <Card className="max-w-2xl bg-transparent shadow-none border-none">
+        <Card className="max-w-[70%] bg-transparent shadow-none border-none">
           <CardContent>
             <span className="prose text-lg pt-8 text-left dark:prose-invert">
               <Markdown remarkPlugins={[remarkGfm]}>{content}</Markdown>
