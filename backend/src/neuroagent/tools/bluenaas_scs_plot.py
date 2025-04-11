9e585066a56091847873697fe884b047cb996b1e
Boris Bergsma <boris.bergsma@openbraininstitute.org>
2025-04-11 15:22:48 +0200

    change LLM behaviour

--- backend/src/neuroagent/tools/bluenaas_scs_plot.py
+++ backend/src/neuroagent/tools/bluenaas_scs_plot.py
@@ -47,7 +47,7 @@ class SCSPlotTool(BaseTool):
     name_frontend: ClassVar[str] = "Plot Single-Neuron Simulation"
     description: ClassVar[str] = """Make a plot of the simulation, based on its id.
     The id can be retrieved using the 'scs-getall-tool', from the simulation report of `scs-post-tool` or directly specified by the user.
-    The plots will be shown after your message automatically. DO NOT mention the storage ids. You can mention the title to refer to the plots."""
+    The plots will be shown after your message automatically. DO NOT mention the storage ids. DO NOT embed the plot in your message."""
     description_frontend: ClassVar[
         str
     ] = """Make a plot from the result of your simulation.
