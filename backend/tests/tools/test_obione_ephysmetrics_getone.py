cbdd7e54010fd236cc319d64bfc358bc639ea5f9
Jan Krepl <18519371+jankrepl@users.noreply.github.com>
2025-06-24 14:29:25 +0200

    Provide a more realistic unit test

--- backend/tests/tools/test_obione_ephysmetrics_getone.py
+++ backend/tests/tools/test_obione_ephysmetrics_getone.py
@@ -20,12 +20,120 @@ class TestEphysMetricsGetOneTool:
             url="http://obione.org/declared/electrophysiology-metrics/1234",
             json={
                 "feature_dict": {
-                    "spike_frequency": {"avg": 15.5, "unit": "Hz", "num_traces": 3},
-                    "resting_membrane_potential": {
-                        "avg": -65.2,
-                        "unit": "mV",
-                        "num_traces": 3,
-                    },
+                    "step_0": {
+                        "spike_count": {
+                            "avg": 1.6666666666666667,
+                            "num_traces": 3,
+                            "unit": None,
+                        },
+                        "time_to_first_spike": {
+                            "avg": 6.62500000002413,
+                            "num_traces": 3,
+                            "unit": "ms",
+                        },
+                        "time_to_last_spike": {
+                            "avg": 19.400000000027035,
+                            "num_traces": 3,
+                            "unit": "ms",
+                        },
+                        "inv_time_to_first_spike": {
+                            "avg": 303.1065437050581,
+                            "num_traces": 3,
+                            "unit": "Hz",
+                        },
+                        "doublet_ISI": {
+                            "avg": 19.162500000004357,
+                            "num_traces": 2,
+                            "unit": "ms",
+                        },
+                        "inv_first_ISI": {
+                            "avg": 72.74608153443701,
+                            "num_traces": 2,
+                            "unit": "Hz",
+                        },
+                        "mean_frequency": {
+                            "avg": 228.74223751569082,
+                            "num_traces": 3,
+                            "unit": "Hz",
+                        },
+                        "strict_burst_number": {
+                            "avg": 0,
+                            "num_traces": 3,
+                            "unit": None,
+                        },
+                        "AP_height": {
+                            "avg": -12.364847343260458,
+                            "num_traces": 3,
+                            "unit": "mV",
+                        },
+                        "AP_amplitude": {
+                            "avg": 50.82487408104208,
+                            "num_traces": 3,
+                            "unit": "mV",
+                        },
+                        "AP1_amp": {
+                            "avg": 52.47725295811486,
+                            "num_traces": 3,
+                            "unit": "mV",
+                        },
+                        "APlast_amp": {
+                            "avg": 49.1724952039693,
+                            "num_traces": 3,
+                            "unit": "mV",
+                        },
+                        "AP_duration_half_width": {
+                            "avg": 0.30833333333340346,
+                            "num_traces": 3,
+                            "unit": "ms",
+                        },
+                        "AHP_depth": {
+                            "avg": 20.406671862343615,
+                            "num_traces": 3,
+                            "unit": "mV",
+                        },
+                        "AHP_time_from_peak": {
+                            "avg": 4.0458333333342535,
+                            "num_traces": 3,
+                            "unit": "ms",
+                        },
+                        "AP_peak_upstroke": {
+                            "avg": 308.75729239209045,
+                            "num_traces": 3,
+                            "unit": "V/s",
+                        },
+                        "AP_peak_downstroke": {
+                            "avg": -187.83831913656505,
+                            "num_traces": 3,
+                            "unit": "V/s",
+                        },
+                        "voltage_base": {
+                            "avg": -84.99950124873655,
+                            "num_traces": 3,
+                            "unit": "mV",
+                        },
+                        "voltage_after_stim": {
+                            "avg": -86.35750426839668,
+                            "num_traces": 3,
+                            "unit": "mV",
+                        },
+                        "ohmic_input_resistance_vb_ssse": {
+                            "avg": 98.15062867785197,
+                            "num_traces": 3,
+                            "unit": "MΩ",
+                        },
+                        "steady_state_voltage_stimend": {
+                            "avg": -48.9862517948066,
+                            "num_traces": 3,
+                            "unit": "mV",
+                        },
+                        "decay_time_constant_after_stim": {
+                            "avg": 2.369212177407707,
+                            "num_traces": 3,
+                            "unit": "ms",
+                        },
+                        "depol_block_bool": {"avg": 0, "num_traces": 3, "unit": None},
+                        "stimulus_current": "0.37648126234610874 nA",
+                    }
                 }
             },
         )
@@ -49,12 +157,120 @@ class TestEphysMetricsGetOneTool:
             url="http://obione.org/declared/electrophysiology-metrics/1234",
             json={
                 "feature_dict": {
-                    "spike_frequency": {"avg": 15.5, "unit": "Hz", "num_traces": 3},
-                    "resting_membrane_potential": {
-                        "avg": -65.2,
-                        "unit": "mV",
-                        "num_traces": 3,
-                    },
+                    "step_0": {
+                        "spike_count": {
+                            "avg": 1.6666666666666667,
+                            "num_traces": 3,
+                            "unit": None,
+                        },
+                        "time_to_first_spike": {
+                            "avg": 6.62500000002413,
+                            "num_traces": 3,
+                            "unit": "ms",
+                        },
+                        "time_to_last_spike": {
+                            "avg": 19.400000000027035,
+                            "num_traces": 3,
+                            "unit": "ms",
+                        },
+                        "inv_time_to_first_spike": {
+                            "avg": 303.1065437050581,
+                            "num_traces": 3,
+                            "unit": "Hz",
+                        },
+                        "doublet_ISI": {
+                            "avg": 19.162500000004357,
+                            "num_traces": 2,
+                            "unit": "ms",
+                        },
+                        "inv_first_ISI": {
+                            "avg": 72.74608153443701,
+                            "num_traces": 2,
+                            "unit": "Hz",
+                        },
+                        "mean_frequency": {
+                            "avg": 228.74223751569082,
+                            "num_traces": 3,
+                            "unit": "Hz",
+                        },
+                        "strict_burst_number": {
+                            "avg": 0,
+                            "num_traces": 3,
+                            "unit": None,
+                        },
+                        "AP_height": {
+                            "avg": -12.364847343260458,
+                            "num_traces": 3,
+                            "unit": "mV",
+                        },
+                        "AP_amplitude": {
+                            "avg": 50.82487408104208,
+                            "num_traces": 3,
+                            "unit": "mV",
+                        },
+                        "AP1_amp": {
+                            "avg": 52.47725295811486,
+                            "num_traces": 3,
+                            "unit": "mV",
+                        },
+                        "APlast_amp": {
+                            "avg": 49.1724952039693,
+                            "num_traces": 3,
+                            "unit": "mV",
+                        },
+                        "AP_duration_half_width": {
+                            "avg": 0.30833333333340346,
+                            "num_traces": 3,
+                            "unit": "ms",
+                        },
+                        "AHP_depth": {
+                            "avg": 20.406671862343615,
+                            "num_traces": 3,
+                            "unit": "mV",
+                        },
+                        "AHP_time_from_peak": {
+                            "avg": 4.0458333333342535,
+                            "num_traces": 3,
+                            "unit": "ms",
+                        },
+                        "AP_peak_upstroke": {
+                            "avg": 308.75729239209045,
+                            "num_traces": 3,
+                            "unit": "V/s",
+                        },
+                        "AP_peak_downstroke": {
+                            "avg": -187.83831913656505,
+                            "num_traces": 3,
+                            "unit": "V/s",
+                        },
+                        "voltage_base": {
+                            "avg": -84.99950124873655,
+                            "num_traces": 3,
+                            "unit": "mV",
+                        },
+                        "voltage_after_stim": {
+                            "avg": -86.35750426839668,
+                            "num_traces": 3,
+                            "unit": "mV",
+                        },
+                        "ohmic_input_resistance_vb_ssse": {
+                            "avg": 98.15062867785197,
+                            "num_traces": 3,
+                            "unit": "MΩ",
+                        },
+                        "steady_state_voltage_stimend": {
+                            "avg": -48.9862517948066,
+                            "num_traces": 3,
+                            "unit": "mV",
+                        },
+                        "decay_time_constant_after_stim": {
+                            "avg": 2.369212177407707,
+                            "num_traces": 3,
+                            "unit": "ms",
+                        },
+                        "depol_block_bool": {"avg": 0, "num_traces": 3, "unit": None},
+                        "stimulus_current": "0.37648126234610874 nA",
+                    }
                 }
             },
         )
