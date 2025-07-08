d21699d61ab12d9b99e67b850df4cc8516f5bf34
Jan Krepl <18519371+jankrepl@users.noreply.github.com>
2025-07-08 13:32:10 +0200

    Make mypy happy

--- backend/src/neuroagent/tools/entitycore_brainregion_getall.py
+++ backend/src/neuroagent/tools/entitycore_brainregion_getall.py
@@ -25,7 +25,7 @@ class BrainRegionGetAllInput(ReadManyBrainRegionGetParametersQuery):
         default=None,
         description="Name of the brain region. Will perform case sensitive exact match. Use for exact names only. The name MUST be capitalized.",
     )
-    name__in: str | None = Field(
+    name__in: list[str] | None = Field(
         default=None,
         description="Comma separated list of multiple brain regions. Each one of them must be capitalized. Does exact match on every single one.",
     )
