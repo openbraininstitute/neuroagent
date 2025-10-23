55c1a634c4cac38535a0d22fe1544760f8205a67
Boris Bergsma <boris.bergsma@openbraininstitute.org>
2025-10-23 10:11:32 +0200

    fix FE linting

--- frontend/src/lib/env.ts
+++ frontend/src/lib/env.ts
@@ -1,16 +1,3 @@
-import { z } from "zod";
-
-const envSchema = z.object({
-  // Server
-  SERVER_SIDE_BACKEND_URL: z.string().url().optional(),
-  NEXTAUTH_SECRET: z.string().min(1),
-  KEYCLOAK_ID: z.string().min(1),
-  KEYCLOAK_SECRET: z.string().min(1),
-  KEYCLOAK_ISSUER: z.string().url(),
-  // Client
-  NEXT_PUBLIC_BACKEND_URL: z.string().url(),
-});
-
 export const env = {
   // Server
   SERVER_SIDE_BACKEND_URL: process.env.SERVER_SIDE_BACKEND_URL,
--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -1,14 +1,7 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 import { MessageStrict, MessageMetadata } from "@/lib/types";
-import {
-  UIMessagePart,
-  ToolUIPart,
-  UITools,
-  UIDataTypes,
-  UIMessage,
-  UITool,
-} from "ai";
+import { UIMessagePart, ToolUIPart, UITools, UIDataTypes, UIMessage } from "ai";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
